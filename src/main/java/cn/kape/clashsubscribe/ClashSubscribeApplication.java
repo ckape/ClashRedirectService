df2f032efe560cd6e65c2039fbd36c4af1c0262d
陈佳鹏 <jpchen2@iflytek.com>
2023-02-20 09:14:27 +0800

    initial project

---
+++ src/main/java/cn/kape/clashsubscribe/ClashSubscribeApplication.java
@@ -0,0 +1,13 @@
+package cn.kape.clashsubscribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClashSubscribeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClashSubscribeApplication.class, args);
+    }
+
+}
